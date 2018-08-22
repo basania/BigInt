bc63dbd35a525003d531358b3597f4edc10ed1ce
basania <basania1996@gmail.com>
2018-08-23 00:58:10 +0300

    Better overflow handling

--- BigInt/BigInteger.h
+++ BigInt/BigInteger.h
@@ -26,19 +26,23 @@ public:
 			int exp = 10;
 			int power = 0;
 			int carry = 0;
-			std::bitset<size> auxiliary;
-			std::bitset<size> sum;
+			std::bitset<size + 1> auxiliary;
+			std::bitset<size + 1> sum;
 			
 			--tmp_sz;
+
 			for (; tmp_sz >= 0; tmp_sz--)
 			{
+				if (s[tmp_sz] != 0)
+					is_zero = false;
+
 				if (isdigit(s[tmp_sz]))
 				{
 					auxiliary = (s[tmp_sz] - 48) * pow(exp, power);
-					for (int i = 0; i < size - 1; i++)
+					for (int i = 0; i < size + 1; i++)
 					{
-						std::bitset<size> tmp = sum;
-
+						std::bitset<size + 1> tmp = sum;
+						
 						sum[i] = tmp[i] ^ auxiliary[i] ^ carry;
 						carry = ((tmp[i] & auxiliary[i]) | (tmp[i] & carry)) | (auxiliary[i] & carry);	// full adder
 					}
@@ -47,25 +51,38 @@ public:
 				else
 				{
 					std::cout << "Exception: invalid string.\n";
+					is_wrong = true;
 				}
 			}
-			number = sum;
+			if (sum[sum.size() - 1] == 1)
+			{
+				std::cout << "Bit overflow in the constructor.\n";
+				is_wrong = true;
+			}
+			else if (str_sz != 0 && sum.to_ulong() == 0 && !is_zero)	/* For cases when '1' is only the MSB (e.g. 256, 512...) */
+			{
+				std::cout << "Bit overflow in the constructor (2).\n";
+				is_wrong = true;
+			}
+			if (!is_wrong)
+			for (int i = 0; i < size + 1; i++)
+				number[i] = sum[i];
 		}
 		
 	}
 
 	void print()
 	{
-		bool is_zero = true;
-		for (int i = 0; i < size; ++i)
+		if (!is_wrong)
 		{
-		/*	if (number[i] != 0)
-				is_zero = false;
-			if (!is_zero)
-				*/
-			std::cout << number[i];
+			for (int i = 0; i < size + 1; ++i)
+			{
+				std::cout << number[i];
+			}
+			std::cout << '\n';
 		}
-		std::cout << '\n';
+		else
+			std::cout << "Something is wrong with the class instance.\n";
 	}
 
 	BigInteger& operator+ (const BigInteger& other)
@@ -80,11 +97,13 @@ public:
 			std::bitset<size> sum;
 			for (int i = 0; i < size - 1; ++i)
 			{
-				sum[i] = this->number[i] ^ other.number[i];
-				if (this->number[i] == 1 && other.number[i] == 1)
+				if (this->number[size - 1] == 1 && other.number[size - 1] == 1)
 				{
-					sum[i + 1] = 1;
+					std::cout << "Overflow in the constructor.\n";
+					break;
 				}
+				sum[i] = this->number[i] ^ other.number[i] ^ carry;
+				carry = ((this->number[i] & other.number[i]) | (this->number[i] & carry)) | (other.number[i] & carry);	// full adder
 			}
 			this->number = sum;
 		}
@@ -98,98 +117,15 @@ public:
 	}
 
 private: 
-	std::bitset<size> number; 
+	std::bitset<size + 1> number; 
 	int str_sz;
 	int m_s_bit;
 	bool is_too_big = false;
-	bool is_negative;
+	bool is_wrong = false;
+	bool is_zero;
 };
 
 
 
 
 
-
-
-/*template<int size>
-class BigInteger
-{
-public:
-	BigInteger() = delete;
-
-
-	explicit BigInteger(const char* s)
-		: str_sz(strlen(s))
-	{
-		if (str_sz > size)
-		{
-			std::cout << "Size overflow\n";
-			is_too_big = true;
-		}
-
-		if (!is_too_big)
-		{
-			number.fill(0);
-			int temp = str_sz;
-			--temp;
-
-			for (int i = size - 1; temp >= 0; --i, --temp)
-			{
-
-				if (!isdigit(s[temp]))
-				{
-					std::cout << "Number is invalid\n";
-					break;
-				}
-				number[i] = s[temp] - 48;
-
-				if (number[i] != 0)
-					m_s_bit = number[i];
-			}
-		}
-
-	}
-
-	void print()
-	{
-		bool is_zero = true;
-		for (int i = 0; i < size; ++i)
-		{
-			if (number[i] != 0)
-				is_zero = false;
-			if (!is_zero)
-				std::cout << number[i];
-		}
-		std::cout << '\n';
-	}
-
-	BigInteger& operator+ (const BigInteger& other)
-	{
-		if (other.str_sz > this->str_sz)
-			std::cout << "Addition will cause an overflow\n";
-		else if (other.str_sz == this->str_sz && this->m_s_bit + m_s_bit > 9)
-			std::cout << "Addition will cause an overflow\n";
-		else
-		{
-			int temp = size - 1;
-			for (int i = temp; i >= 0; --temp, --i)
-			{
-				this->number[i] += other.number[i];
-			}
-		}
-		return *this;
-
-	}
-
-	~BigInteger()
-	{
-
-	}
-
-private:
-	std::array<int, size> number;
-	int str_sz;
-	int m_s_bit;
-	bool is_too_big = false;
-};
-*/
